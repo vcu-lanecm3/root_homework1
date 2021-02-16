cc55b69dedc8c1cd94f19d9dfcc92320cfecec65
vcu-Lanecm3 <lanecm3@vcu.edu>
2021-02-15 21:49:05 -0500

    too many blank lines man

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -4,14 +4,12 @@
 def return_number_3():
     """ This function should return an integer with the value of 3"""
 
-
     return_value = 3
     return return_value
 
 
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
-
 
     return_value = "vcu"
     return return_value
@@ -21,7 +19,6 @@ def return_lowercased_string(input_string):
     """You have a variable called input_string that is of type string.
     Return it but the lowercase version of it."""
 
-
     return_value = input_string.lower()
     return return_value
 
@@ -30,14 +27,12 @@ def return_without_starting_ending_whitespace(input_string):
     """You have a variable called input_string that is of type string.
     Return it but with the surrounding (left and right) whitespace stripped."""
 
-
     return_value = input_string.strip()
     return return_value
 
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
-
 
     return_value = first_number + second_number
     return return_value
